5dbaf0fea1003074a57be025857737b104b2bc84
user <user@Nitish-Vermas-Mac-mini.local>
2012-09-13 18:51:51 +0530

    auth added with acl

---
+++ app/Model/User.php
@@ -0,0 +1,92 @@
+<?php
+    App::uses('AuthComponent', 'Controller/Component');
+/**
+ * User Model
+ *
+ * @property Group $Group
+ */
+class User extends AppModel {
+
+  public $name = 'User';  
+  public $actsAs = array('Acl' => array('type' => 'requester'));  
+    
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'username' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'password' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'group_id' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * belongsTo associations
+ *
+ * @var array
+ */
+	public $belongsTo = array('Group');
+		
+
+        
+        
+        
+        public function parentNode() { 
+            if (!$this->id && empty($this->data)) {
+                return null;
+            } 
+            
+            if (isset($this->data['User']['group_id'])) {
+                $groupId = $this->data['User']['group_id']; 
+                
+            } else {
+                $groupId = $this->field('group_id');
+            }        
+        } 
+        
+        
+        
+        public function bindNode($user) { 
+            return array(
+                       'model' => 'Group', 
+                       'foreign_key' => $user['User']['group_id']);
+    }   
+    
+    
+        public function beforeSave($options = array()) { 
+            $this->data['User']['password'] = AuthComponent::password($this->data['User']['password'])   ; 
+             return true;
+        }
+    
+        
+}
---
+++ app/Test/Fixture/UserFixture.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * UserFixture
+ *
+ */
+class UserFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'username' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 100, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'password' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 100, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'groupid' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'created' => array('type' => 'timestamp', 'null' => false, 'default' => 'CURRENT_TIMESTAMP'),
+		'modified' => array('type' => 'timestamp', 'null' => false, 'default' => '0000-00-00 00:00:00'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'username' => 'Lorem ipsum dolor sit amet',
+			'password' => 'Lorem ipsum dolor sit amet',
+			'groupid' => 1,
+			'created' => 1347527340,
+			'modified' => 1347527340
+		),
+	);
+
+}
